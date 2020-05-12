d082c22890287fd14bbb75392a41f0ff36a0c8ca
anaskhan045 <63488883+anaskhan045@users.noreply.github.com>
2020-05-12 23:03:34 +0500

    Add files via upload

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,42 @@
+class ChatModel{
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name,this.message,this.time,this.avatarUrl});
+}
+ 
+ List<ChatModel> dummyData = [
+ new ChatModel(
+   name: "Anas khan",
+   message: "Hey meno, i Love you,mhuaaaa",
+   time: "21:15",
+   avatarUrl: "https://www.flickr.com/photos/141142509@N06/42411733814",
+ ),
+ new ChatModel(
+   name: "Anas khan",
+   message: "Hey meno, i Love you,mhuaaaa",
+   time: "21:15",
+   avatarUrl: "https://www.flickr.com/photos/141142509@N06/42411733814",
+ ),
+ new ChatModel(
+   name: "Anas khan",
+   message: "Hey meno, i Love you,mhuaaaa",
+   time: "21:15",
+   avatarUrl: "https://www.flickr.com/photos/141142509@N06/42411733814",
+ ),
+ new ChatModel(
+   name: "Anas khan",
+   message: "Hey meno, i Love you,mhuaaaa",
+   time: "21:15",
+   avatarUrl: "https://www.flickr.com/photos/141142509@N06/42411733814",
+ ),new ChatModel(
+   name: "Anas khan",
+   message: "Hey meno, i Love you,mhuaaaa",
+   time: "21:15",
+   avatarUrl: "https://www.flickr.com/photos/141142509@N06/42411733814",
+ ),
+
+ ];
+
